0bcaf8b479b440c3ecd4fa25b7616eec9722c27d
paarandika <paarandika@gmail.com>
2015-11-08 03:43:28 +0530

    Url extraction completed

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-from parser import *
+from hparser import HTMLUrlParser
 from urllib.parse import urlparse, urljoin
 from urllib.request import Request, urlopen
 
@@ -6,33 +6,56 @@ class UrlObj:
     def __init__(self, url, level):
         self.url=url
         self.level=level
+
+url_keeper=[]
+
 def url_extractor(url, page, level):
-        parser=HTMLUrlParser()
-        parser.feed(page)
-        out=[]
-        for item in parser.url_list:
-            temp=UrlObj(urljoin(url, item), level)
+    p= HTMLUrlParser()
+    p.feed(page)
+    out=[]
+    for item in p.url_list:
+        if "#" in item:
+            turl=item[0:item.index('#')]
+        else:
+            turl=item
+        turl=urljoin(url, turl)
+        parsed_turl = urlparse(turl)
+        parsed_base=urlparse(url_keeper[0])
+        print(turl)
+        if turl not in url_keeper and parsed_turl.netloc==parsed_base.netloc:
+            url_keeper.append(turl)
+            temp=UrlObj(turl, level)
             out.append(temp)
-        return out
+    return out
 
 def get_page(url):
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    webpage = str(urlopen(req).read())
-    return webpage
+    print(url)
+    try:
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = str(urlopen(req).read())
+        return webpage
+    except:
+        print("Could not get "+url)
+        return ""
 
 def content_extractor():
     pass
 
 def scrape (url, levels, folder):
+    global url_keeper
     parsed_uri = urlparse(url)
     file_name=folder+"/"+parsed_uri.netloc
+    url_keeper=[]
     index=[]
+    url_keeper.append(url)
     index.append(UrlObj(url,1))
     ticket=0
     while not index==[]:
         ticket+=1
         temp=index.pop()
         page=get_page(temp.url)
-        if temp.level<levels:
-            index=index+url_extractor(url, page, temp.level+1)
+        if not page=="":
+            if temp.level<levels:
+                index=index+url_extractor(url, page, temp.level+1)
+
 
