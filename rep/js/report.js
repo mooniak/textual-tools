d3042b15296552ddf1bdb338954d7deb8369d3f9
paarandika <paarandika@gmail.com>
2015-11-26 01:51:31 +0530

    formatted report.js

--- rep/js/report.js
+++ rep/js/report.js
@@ -1,65 +1,65 @@
 ﻿$(function () {
-    var data=function(lables, vals){
-        var out={
+    var data = function (lables, vals) {
+        var out = {
             labels: lables,
             series: [
                 vals
             ]
         };
         return out;
-    }
+    };
 
-    var options=function(ledger){
+    var options = function (ledger) {
         var out = {
             reverseData: true,
             horizontalBars: true,
             fullWidth: true,
-            showArea:true,
-            height:ledger.length*20+'px',
+            showArea: true,
+            height: ledger.length * 20 + 'px',
             axisY: {
                 offset: 70
             }
         };
         return out
-    }
+    };
 
-    $('#charachters').html("Charachters : "+charCount);
-    $('#charachters_s').html("Charachters without white spaces : "+charCountNoWhiteSpace);
-    $('#words').html("words : "+wordCount);
-    $('#range').html("range : "+unicodeRange);
-    $('#source').html("source : <a href="+source+"'>"+source+"</a>");
-    $('#files').html("files :  <a href="+name+"'>"+name+"</a>");
-    $('#single').css('height',ledgerSingleLables.length*20);
-    $('#double').css('height',ledgerDoubleLables.length*20);
-    $('#triple').css('height',ledgerTripleLables.length*20);
-    new Chartist.Bar('#single', data(ledgerSingleLables,ledgerSingleVals), options(ledgerSingleLables));
-    new Chartist.Bar('#double', data(ledgerDoubleLables,ledgerDoubleVals), options(ledgerDoubleLables));
-    new Chartist.Bar('#triple', data(ledgerTripleLables,ledgerTripleVals), options(ledgerTripleLables));
+    $('#charachters').html("Charachters : " + charCount);
+    $('#charachters_s').html("Charachters without white spaces : " + charCountNoWhiteSpace);
+    $('#words').html("words : " + wordCount);
+    $('#range').html("range : " + unicodeRange);
+    $('#source').html("source : <a href=" + source + "'>" + source + "</a>");
+    $('#files').html("files :  <a href=" + name + "'>" + name + "</a>");
+    $('#single').css('height', ledgerSingleLables.length * 20);
+    $('#double').css('height', ledgerDoubleLables.length * 20);
+    $('#triple').css('height', ledgerTripleLables.length * 20);
+    new Chartist.Bar('#single', data(ledgerSingleLables, ledgerSingleVals), options(ledgerSingleLables));
+    new Chartist.Bar('#double', data(ledgerDoubleLables, ledgerDoubleVals), options(ledgerDoubleLables));
+    new Chartist.Bar('#triple', data(ledgerTripleLables, ledgerTripleVals), options(ledgerTripleLables));
 });
 
-function textReport(data){
-    ledgerSingleLables=[]
-    ledgerSingleVals=[]
-    ledgerDoubleLables=[]
-    ledgerDoubleVals=[]
-    ledgerTripleLables=[]
-    ledgerTripleVals=[]
-    $.each(data.ledgerSingle, function(key, load){
+function textReport(data) {
+    ledgerSingleLables = [];
+    ledgerSingleVals = [];
+    ledgerDoubleLables = [];
+    ledgerDoubleVals = [];
+    ledgerTripleLables = [];
+    ledgerTripleVals = [];
+    $.each(data.ledgerSingle, function (key, load) {
         ledgerSingleVals.push(load.count);
         ledgerSingleLables.push(load.character);
     });
-    $.each(data.ledgerDouble, function(key, load){
+    $.each(data.ledgerDouble, function (key, load) {
         ledgerDoubleVals.push(load.count);
         ledgerDoubleLables.push(load.character);
     });
-    $.each(data.ledgerTriple, function(key, load){
+    $.each(data.ledgerTriple, function (key, load) {
         ledgerTripleVals.push(load.count);
         ledgerTripleLables.push(load.character);
     });
-    charCount=data.charCount;
-    charCountNoWhiteSpace=data.charCountNoWhiteSpace;
-    wordCount=data.wordCount;
-    unicodeRange=data.unicodeRange;
-    name=data.name;
-    source=data.source;
+    charCount = data.charCount;
+    charCountNoWhiteSpace = data.charCountNoWhiteSpace;
+    wordCount = data.wordCount;
+    unicodeRange = data.unicodeRange;
+    name = data.name;
+    source = data.source;
 }
